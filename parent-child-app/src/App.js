cd75b21c4ef46e96119dcbfaa5ff24fa36bd3af6
Imesha Madhumali <139534200+ImeshaMadhumali@users.noreply.github.com>
2024-11-21 10:57:42 +0000

    Add state management for childname in App component

--- parent-child-app/src/App.js
+++ parent-child-app/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [childname, setChildname] = useState("Default");
   return (
     <div className="App">
      <h2>Hello React I'm Parent</h2>
+     <p>Parent: My child said: {childname}</p>
     </div>
   );
 }
